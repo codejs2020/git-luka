bb2a0c1da4fef5729804bdc998a563bb4c557667
lukautvic <67318904+lukautvic@users.noreply.github.com>
2020-07-10 23:19:26 +0200

    Test 4

--- src/js/lf2crlf.js
+++ src/js/lf2crlf.js
@@ -2,5 +2,8 @@
 const a = ''
 // eslint-disable-next-line no-console
 console.log(a)
+const b = ''
+// eslint-disable-next-line no-console
+console.log(b)
 /* eslint-disable no-unused-vars */
 // Test 2
